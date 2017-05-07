802f4c28969b67f531219e4dae2e2948763e9cb0
John Yu <jlumongsod@gmail.com>
2017-05-07 22:58:07 +0800

    Coding done. UT phase next. (modified files)

--- demoproject/src/pages/auth/auth.ts
+++ demoproject/src/pages/auth/auth.ts
@@ -1,20 +1,76 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ProjPage } from '../proj/proj';
+import { AlertController } from 'ionic-angular';
+
+import { AuthService } from '../../app/services/auth.service';
 
 @Component({
   selector: 'auth',
   templateUrl: 'auth.html'
 })
 export class AuthPage {
-
-  constructor(public navCtrl: NavController) {
+  loginOK: boolean;
+  errorMsg: string;
+  constructor(public navCtrl: NavController, 
+              public alertCtrl: AlertController, 
+              private authService: AuthService) {
 
   }
 
+  //
+  //Method for overall validation using authService APIs
+  //Composes appropriate dialog message content
+  //
   validateAuth(){
-      //validate email and password
-      this.navCtrl.push(ProjPage);
+      //initialize status
+      this.loginOK = true;
+      this.errorMsg = "";
+      
+      //validate missing email
+      if( this.authService.validateRequired(document.forms["loginForm"]["femail"].value)){
+        this.errorMsg = this.errorMsg + "<br>Input missing: Email";
+        this.loginOK = false;
+      } 
+      //validate email format
+      else if( this.authService.validateEmailFormat(document.forms["loginForm"]["femail"].value)){
+        this.errorMsg = this.errorMsg + "<br>Incorrect Email format";
+        this.loginOK = false;
+      }
+
+      //validate missing password
+      if( this.authService.validateRequired(document.forms["loginForm"]["fpassword"].value)){
+        this.errorMsg = this.errorMsg + "<br>Input missing: Password";
+        this.loginOK = false;
+      }
+      //validate password length
+      else if( this.authService.validatePwdLen(document.forms["loginForm"]["fpassword"].value)){
+        this.errorMsg = this.errorMsg + "<br>Password must have at least " + this.authService.MIN_PWD_LENGTH + " characters";
+        this.loginOK = false;        
+      }
+  
+      //DEBUGGING: Disable authentication/validation. Restore before Code Release
+      //if( !this.loginOK ){
+      
+      //check status to proceed
+      if( this.loginOK ){
+          this.navCtrl.push(ProjPage);
+      } else {
+        this.showAlert(this.errorMsg);
+      }
+
+  }
+
+  //
+  //Method to display dialog message
+  //
+  showAlert(msg: any) {
+    let alert = this.alertCtrl.create({
+      title: 'Authentication Failed',
+      subTitle: msg,
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
--- demoproject/src/pages/register/register.ts
+++ demoproject/src/pages/register/register.ts
@@ -1,20 +1,64 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ProjPage } from '../proj/proj';
+import { AlertController } from 'ionic-angular';
+
+import { RegisterService } from '../../app/services/register.service';
 
 @Component({
   selector: 'register',
   templateUrl: 'register.html'
 })
 export class RegisterPage {
-
-  constructor(public navCtrl: NavController) {
+  projects: string[];
+  
+  constructor(public navCtrl: NavController,
+              public alertCtrl: AlertController,
+              private registerService: RegisterService) {
 
   }
 
-  registerProj(){
-    //check for uniqueness of project name
-    //if unique, store in localStorage
-    this.navCtrl.push(ProjPage);
+  //
+  //Method upon pressing Register button
+  //
+  registerProj(project){
+    this.projects = this.registerService.getProjList();
+    
+    //check if duplicate project name
+    if (this.registerService.checkDuplicate(project)){     
+      this.showAlert("Project Name already exists! Please try again.");
+    } else {  
+      //store to project list
+      this.projects.push(project);
+      this.registerService.setProjList(this.projects);
+
+      //go to Project Page
+      this.navCtrl.push(ProjPage);
+    }
+    
+    //DEBUGGING: Monitor inputted project name and project list
+    //console.log("@register.ts - input value: " + project);
+    //console.log("@register.ts - registerProj(): " + this.registerService.showProjList());
   }
+
+  //
+  //Method upon pressing Cancel button
+  //
+  cancel(){
+    //go back to Previous Page
+    this.navCtrl.pop();
+  }
+
+  //
+  //Method to display dialog message
+  //
+  showAlert(msg: any) {
+      let alert = this.alertCtrl.create({
+      title: 'Registration Failed',
+      subTitle: msg,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+    
 }
